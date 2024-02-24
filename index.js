8a74e393c3c9f4984f35cb66f52dd58bd3a9e3e6
Rachit Singhal <singhalrachit100@gmail.com>
2024-02-24 11:39:31 +0530

    updates files

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.send("<h1>hello world</h1>");
-})
-app.listen(port,()=>{
-    console.log(`Server running on this ${port} number`);
-})
+app.get('/', (req, res) => {
+    res.render("index", { message: "hello world" });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
